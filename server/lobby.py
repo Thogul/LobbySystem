84a5ab54a346b00c54bc29e398efaf2a442785a7
Krabbefett <erikfp@hotmail.no>
2020-07-20 21:25:12 +0200

    Update lobby.py

--- server/lobby.py
+++ server/lobby.py
@@ -1,6 +1,6 @@
 class Lobby():
-    def __init__(self, lobby_code, user=None):
-        self.lobby_code = lobby_code
+    def __init__(self, lobby_id, user=None):
+        self.lobby_id = lobby_id
         if user:
             self.users = [user]
             self.connected = 1
@@ -17,4 +17,4 @@ class Lobby():
             self.users.remove(user)
 
     def __str__(self):
-        return self.lobby_code
+        return self.lobby_id
