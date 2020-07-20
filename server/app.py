b82f2e8ff33199afdcc5bb131f4d860c3deb3765
Thogul <--get>
2020-07-20 21:55:00 +0200

    lobbykeygen and stuff

--- server/app.py
+++ server/app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, jsonify, request, send_from_directory
-from flask_cors import CORS
+from flask_cors import CORS #Cross site rescource sharing stuff idk, it works
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
-import ast
 
 #local imports:
-from game_room import Game_room
+# Should change to lobby class later
+from lobby import Lobby
+#for generating lobby keys
+from key_gen import Key_gen
 
+#Instansiating app server and adding socketIO finctionality
 app  = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app)
 
-#adding debug config
+#app server Configs
 app.config["DEBUG"] = True
 
+#Temp for testing
 users = ["thomas", "lars", "thogul", "larsaur"]
 
-lobbies = [Game_room("test")]
+#list of lobbies that are live
+lobbies = [Lobby("test")]
+
+lobby_key_gen = Key_gen() #for making lobby IDs
 
 @app.route("/")
 def index():
-    """Here we want to show lobbies and stuff"""
+    """Serves the main page for creating and joining lobbies"""
     return send_from_directory("../client/", "index.html")
 
 @app.route("/src/<path:filename>")
@@ -28,39 +35,21 @@ def src_files(filename):
     path = filename.split("/")
     return send_from_directory("../client/src/" + "/".join(path[:-1]), path[-1])
 
-@app.route("/users")
-def get_users():
-    info = { "users" : users}
-    return jsonify(info)
-
-@app.route("/user/<username>", methods=['GET', 'POST'])
-def get_user(username):
-    """Maybe intersting later idk"""
-    info = {}
-    if request.method == 'POST':
-        data = request.json
-        name = data["username"]
-        if name is not None and name not in users:
-            users.append(name)
-            info["info"] = f"added user {name}"
-        else:
-            info["info"] = "error adding user"
-    else:
-        if username in users:
-            info["info"] = "this user exists"
-            info["user"] = username
-            info["len"] = len(username)
-        else:
-            info["info"] = "This user does not exist"
-
-    return jsonify(info)
-
+@app.route("/create", methods = ['POST'])
+def create_lobby():
+    ##data = request.json
+    lobbyID = lobby_key_gen.new_key()
+    lobbies.append(Lobby(lobbyID))
+    return jsonify({'lobbyID' : lobbyID, 'success' : True})
+    
 @socketio.on('message')
 def handle_message(message):
+    """Idk what we are using this for right now, maybe for later"""
     print('recieved message: ' + message)
 
 @socketio.on('connect')
 def handle_connect():
+    """If we want to do something when a device connects to the lobbies"""
     print("device connected")
     emit('my response', {'data' : 'Connected!'})
 
@@ -69,10 +58,10 @@ def handle_connect():
 def on_join(data):
     username = data['username']
     users.append(username)
-    room = data['room']
-    if room in lobbies:
-        join_room(room)
-        send(username + ' Has enterd the room', room=room)
+    lobby = data['room']
+    if lobby in lobbies:
+        join_room(lobby)
+        send(username + ' Has enterd the room', room=lobby)
     else:
         emit('my response', {'data' : 'Room does not exist'})
 
---
+++ server/key_gen.py
@@ -0,0 +1,16 @@
+from random import randint
+
+class Key_gen():
+    def __init__(self, max_len : int = 5):
+        self.max_len = max_len
+        self.count = 0
+
+    def new_key(self) -> str:
+        characters = "abcdefghijklmnopqrstuvwxyz1234567890"
+        code = ""   
+        for _ in range(self.max_len):
+            ch = randint(0, len(characters))
+            code += characters[ch]
+        
+        self.count += 1
+        return code
