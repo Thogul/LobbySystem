9c6ec065828ffb7064c635cab2a041b12a808943
Krabbefett <erikfp@hotmail.no>
2020-07-20 21:56:59 +0200

    Update lobby
    Konstruerer spillere i join()

--- server/lobby.py
+++ server/lobby.py
@@ -1,22 +1,19 @@
-from player import Player
+from game.player import Player
 
 class Lobby():
-    def __init__(self, lobby_id, player=None):
+    def __init__(self, lobby_id):
         self.lobby_id = lobby_id
-        if player:
-            self.players = [player]
-            self.connected = 1
-        else:
-            self.players = []
+        self.players = []
     
-    def join(self, player):
-        self.connected += 1
-        self.players.append(player)
+    def join(self, playerName):
+        newPlayer = Player(playerName, len(self.players))
+        self.players.append(newPlayer)
     
     def leave(self, player):
         if player in self.players:
-            self.connected -= 1
             self.players.remove(player)
 
     def __str__(self):
         return self.lobby_id
+
+
