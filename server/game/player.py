133050613924f74564ac8d19b65bead696d75ca8
Krabbefett <erikfp@hotmail.no>
2020-07-20 21:40:27 +0200

    player og variabelskifte
    Lagde player-class og endret variabelnavn "user" til "player" i lobby

---
+++ server/game/player.py
@@ -0,0 +1,8 @@
+class Player():
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    
+    def __str___(self):
+        return self.name
+        
--- server/lobby.py
+++ server/lobby.py
@@ -1,20 +1,22 @@
+from player import Player
+
 class Lobby():
-    def __init__(self, lobby_id, user=None):
+    def __init__(self, lobby_id, player=None):
         self.lobby_id = lobby_id
-        if user:
-            self.users = [user]
+        if player:
+            self.players = [player]
             self.connected = 1
         else:
-            self.users = []
+            self.players = []
     
-    def join(self, user):
+    def join(self, player):
         self.connected += 1
-        self.users.append(user)
+        self.players.append(player)
     
-    def leave(self, user):
-        if user in self.users:
+    def leave(self, player):
+        if player in self.players:
             self.connected -= 1
-            self.users.remove(user)
+            self.players.remove(player)
 
     def __str__(self):
         return self.lobby_id
