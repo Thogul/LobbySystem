1221f266cadbbcafd4dc54ebe74d9ce22e836cb1
LarsAur <lars1998@hotmail.com>
2020-08-01 18:48:33 +0200

    created form and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # LobbySystem
 Learning to create a lobby system
 
-# React
-To use react, install node.js (LTS) <https://nodejs.org/en/>
-Install dependecies by opening a terminal in the client folder and run 'npm install'
-Run the react app by running 'npm start'
+# React and Front-End Dependencies
+* To use react, install node.js (LTS) <https://nodejs.org/en/>
+* Install dependecies by opening a terminal in the client folder and run `npm install`. This will automatically install all dependencies from the package.json
+* Build the react app by running `npm run build`.
+
+# Flask and Back-End Dependencies
+* The server requires python <https://www.python.org/downloads/> (Make sure to install pip)
+* Install the requirements by running `pip install -r requirements.txt` in the server folder
+* Run the Flask server with `python app.py` from the server folder
+* The server will be hosted at <localhost:5000>
--- client/src/screens/lobby.js
+++ client/src/screens/lobby.js
@@ -1,48 +1,103 @@
 import React from "react"
-import Button from '@material-ui/core/Button';
 
 import Network from '../network'
+import './styles/lobby.css'
 
 export default class Lobby extends React.Component{
-    
-    state = {
-        host: undefined,
-        socket: undefined
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: '',
+            lobby_code: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    /*constructor(){
-        super();
-    }*/
+    // Handles change event in the form, it is only made to handle type=text
+    handleChange(event){
+        const target = event.target
+        const name = target.name
+        const value = target.value
+        
+        this.setState({[name]: value})
+    }
+
+    // Handles what happens when form is submited
+    handleSubmit(event){
+        const target = event.target
+        const name = target.name
+
+        if(name === "create_new_lobby"){
+            if(this.validate_name()){
+                Network.create_lobby(this.state.name)
+                console.log(this.state.name)
+            }
+        }
+        else if (name === "join_lobby"){
+            if(this.validate_name() && this.validate_lobby_code()){
+                Network.join_lobby(this.state.name, this.state.lobby_code)
+                console.log(this.state.name)
+            }
+        }
+        // Stops form from beeing submitted as default
+        event.preventDefault()
+    }
+
+    validate_name(){
+        return true
+    }
+
+    validate_lobby_code(){
+        return true
+    }
 
     render(){
         return(
-            <div>
-            <header>
-                <h1>Welcome to lobby creator</h1>
-            </header>
+            <div className="login-form">
+                <form onSubmit={this.handleSubmit}>
+                    <label> <b>Name:</b>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            value={this.state.name} 
+                            onChange={this.handleChange} 
+                            placeholder="TumbleweedMaster..." 
+                            autoFocus
+                        />
+                    </label>
+                    <br/>
+                    <label> <b>Lobby Code:</b>
+                        <input 
+                            type="text" 
+                            name="lobby_code" 
+                            value={this.state.lobby_code} 
+                            onChange={this.handleChange} 
+                            maxLength="5" 
+                            placeholder="DJLQP"
+                        />
+                    </label>
+
+                    <input type="submit" name="create_new_lobby" value="Create New Lobby"/>
+                    <input type="submit" name="join_lobby" value="Join Lobby"/>
+                </form>
 
             <br/>
-            <b>Lobby code</b>
-            <input id="lobby-input" type="text"/>
-
-            <br/>
-            <b>Your name</b>
-            <input id="name-input" type="text"/>
-
-            <br/>
-            <Button 
+            {/*<Button 
                 variant="contained"
                 color="primary" 
                 onClick={() => Network.create_lobby("test_name")}>
                     Create New Lobby
             </Button>
-            <br/>
             <Button 
                 variant="contained" 
                 color="primary" 
                 onClick={() => Network.join_lobby("test_name", "test_name2")}>
                     Join Lobby
-            </Button>
+            </Button>*/}
             </div>
         )
     }
